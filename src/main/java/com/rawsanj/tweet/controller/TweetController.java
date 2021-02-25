9c93f71a48bb95e6494dd141e7f534ea08f5049b
yennanliu <f339339@gmail.com>
2021-02-26 00:58:13 +0800

    update TweetController.java, /templates/resultPage.html

--- src/main/java/com/rawsanj/tweet/controller/TweetController.java
+++ src/main/java/com/rawsanj/tweet/controller/TweetController.java
@@ -4,11 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.social.config.annotation.EnableSocial;
 import org.springframework.social.twitter.api.SearchResults;
+import org.springframework.social.twitter.api.Tweet;
 import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/")
@@ -26,8 +30,15 @@ public class TweetController {
     public String helloTwitter(@RequestParam(defaultValue = "ETF") String search, Model model){
         SearchResults searchResults = twitterImpl.searchOperations().search(search);
         System.out.println("searchResults = " + searchResults);
-        String tweets = searchResults.getTweets().get(0).getText();
+
+        List<String> tweets =
+                searchResults.getTweets()
+                .stream()
+                .map(Tweet::getText)
+                .collect(Collectors.toList());
+        //String tweets = searchResults.getTweets().get(0).getText();
         //String tweets = "etf !!!";
+
         System.out.println("tweets = " + tweets);
         model.addAttribute("tweets", tweets);
         return "resultPage";
